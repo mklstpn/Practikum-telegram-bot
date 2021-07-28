0fe7e06009d3459f47731a24914237b138fb2f61
Mikhail Stepanov <slake6@yandex.ru>
2021-07-28 12:54:20 +0300

    stage3

--- homework.py
+++ homework.py
@@ -14,74 +14,69 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-statuses = {'reviewing': 'На проверке.',
+BOT = telegram.Bot(token=TELEGRAM_TOKEN)
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+VERDICTS = {'reviewing': 'На проверке.',
             'rejected': 'К сожалению, в работе нашлись ошибки.',
             'approved': 'Ревьюеру всё понравилось, работа зачтена!'}
-ANSWER_TEXT = 'У вас проверили работу "{name_key}"!\n\n{verdict_key}'
+ANSWER_TEXT = 'У вас проверили работу "{name}"!\n\n{verdict}'
+BOT_START_MSG = 'Bot started'
+NO_HOMEWORKS_MSG = 'Homeworks list is empty'
+ERROR_MSG = 'Error: '
 
 
 def parse_homework_status(homework):
     name = homework.get('homework_name')
     status = homework.get('status')
-    logging.debug(f'Homework name - {name}')
-    logging.debug(f'Homework status - {status}')
     try:
-        verdict = statuses[status]
-    except KeyError:
-        raise KeyError('Got unkown status')
-    logging.debug(f'Homework check result - {verdict}')
-    return ANSWER_TEXT.format(name_key=name, verdict_key=verdict)
+        verdict = VERDICTS[status]
+    except ValueError as value_error:
+        raise ValueError(ERROR_MSG + str(value_error))
+    return ANSWER_TEXT.format(name=name, verdict=verdict)
 
 
 def get_homeworks(current_timestamp):
     payload = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get(
-            url, headers=headers, params=payload).json()
-    except ConnectionError:
-        raise ConnectionError('Connection error')
-    except TypeError:
-        raise TypeError('Got wrong data')
-    logging.debug(f'Check JSON PRAKTIKUM - {homework_statuses}')
+        homework_get = requests.get(
+            URL, headers=HEADERS, params=payload)
+    except requests.exceptions.RequestException as request_error:
+        raise ConnectionError(ERROR_MSG + str(request_error))
+    homework_statuses = homework_get.json()
+    # Но в получаемом JSON'e ведь нет ни "code" ни "error".
+    # Или в чем-то подвох?
     return homework_statuses
 
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, message)
+    return BOT.send_message(CHAT_ID, message)
 
 
 def main():
     current_timestamp = int(time.time())
 
-    logging.info('Bot starting')
-    logging.info(bot)
+    logging.info(BOT_START_MSG)
+    logging.info(BOT)
     while True:
         try:
             # timestamp for testing 1625764144
-            current_timestamp = current_timestamp or int(time.time())
             homework = get_homeworks(current_timestamp)
             if len(homework['homeworks']) >= 1:
                 homework = homework['homeworks'][0]
-                current_timestamp = homework.get('date_updated')
-                current_timestamp = time.mktime(
-                    dt.datetime.strptime(
-                        current_timestamp, "%Y-%m-%dT%H:%M:%SZ").timetuple())
-                message = parse_homework_status(homework)
-                send_message(message)
+                current_timestamp = homework.get(
+                    'current_date', int(time.time()))
+                send_message(parse_homework_status(homework))
             else:
-                logging.info('Homeworks list is empty')
+                logging.info(NO_HOMEWORKS_MSG)
             time.sleep(5 * 60)
 
         except Exception as error:
-            logging.error('Error: ' + str(error), exc_info=True)
-            time.sleep(5 * 60)
+            logging.error(ERROR_MSG + str(error), exc_info=True)
             try:
-                send_message('Bot died. You need to help him.')
-            except ConnectionError:
-                logging.error('Message not sent')
+                send_message(ERROR_MSG + str(error))
+            except ConnectionError as connect_error:
+                logging.error(ERROR_MSG + str(connect_error))
 
 
 if __name__ == '__main__':
